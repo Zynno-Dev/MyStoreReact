565be4da82a0e4a1ecbb993db186f7724e17dce0
Brian Michelin <michelinbriandev@gmail.com>
2021-07-29 22:16:19 -0300

    first commit

---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./logo.svg";
+import './Navbar.css';
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-secondary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src={logo}
+              alt=""
+              width="30"
+              height="24"
+              className="d-inline-block align-text-top"
+            />
+            CoderHouse React
+          </a>
+          <ul className="tofloat">
+            <li>
+              <a href="">Productos</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar
